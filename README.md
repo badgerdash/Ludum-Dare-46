c781fea71e0cc56746e430c773e12c18843ccd1f
@badgerdash <badgerdash.ua@gmail.com>
2020-04-18 01:16:32 +0300

    Inital Commit. Kha haxe framework.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ludum Dare #46 Game
+
+via [Badgerdash][1b7caf56]
+
+  [1b7caf56]: https://ldjam.com/users/badgerdash/ "Badgerdash @ LDJam"
---
+++ khafile.js
@@ -0,0 +1,5 @@
+let project = new Project('Ludum Dare #46');
+project.addAssets('Assets/**');
+project.addShaders('Shaders/**');
+project.addSources('Sources');
+resolve(project);
